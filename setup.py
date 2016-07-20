1548c367e586e40adba7d03c6141d5aa86c6c7df
willpatterson <wpatt2@pdx.edu>
2016-07-20 14:57:23 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+setup(name="MapSam",
+      version="0.0.0",
+      description="Pulls relivent data from .sam files and maps sam sequences onto a .gff file",
+      license="MIT",
+      author="William Patterson, Amie Romney",
+      packages=find_packages(),
+      install_requires=["BioPython"])
+

615627fcfe9da75723a7c90bfbad76c251f5187b
willpatterson <wpatt2@pdx.edu>
2016-07-21 18:30:16 -0700

    Changed generator __next__() function call to the next() fucntion. Also add testing exception handling"

--- samlink/samlink.py
+++ samlink/samlink.py
@@ -19,32 +19,46 @@ class RegionMap(object):
         self.subregion_types = subregion_types
         self.rmap = self.read_gff(gff_path)
 
-    @staticmethod
-    def read_gff(gff_path, feature_types):
+    @classmethod
+    def read_gff(cls, gff_path, feature_types):
         """Reads a gff file into a region map hash"""
         region_map = {}
-        feature_template = {key: [] for key in feature_types}
-        print(feature_template)
+        feature_temp = {key: [] for key in feature_types}
+        print(feature_temp)
         with open(gff_path, 'r') as gff:
-            for line in gff:
-                if line.startswith('#'):
-                    continue
-                line_gen = split_gen(line, '\t')
-                #print([x for x in line_gen])
-                region_name = line_gen.__next__()
-                line_gen.__next__()
-                feature = line_gen.__next__()
-                if feature == 'region':
-                    region_map.setdefault(region_name, RegionMap.Region(region_name, feature_template, line_gen.__next__(), line_gen.__next__()))
-                elif feature in feature_types:
-                    #try:
-                    region_map[region_name].subregions[feature].append([int(line_gen.__next__()), int(line_gen.__next__())]) #except: #TODO
-                    #print('warning') #TODO
-        return region_map
+            for count, line in enumerate(gff):
+                try:
+                    if line.startswith('#'):
+                        print('# line: {}'.format(count))
+                        continue
+                    line_gen = split_gen(line, '\t').next
+                    #print([x for x in line_gen])
+                    region_name = line_gen()
+                    line_gen()
+                    feature = line_gen()
+                    if feature == 'region':
+                        region_map.setdefault(region_name,
+                                              cls.Region(region_name,
+                                                         feature_temp,
+                                                         int(line_gen()),
+                                                         int(line_gen())))
+                    elif feature in feature_types:
+                        try:
+                            region_map[region_name].subregions[feature]\
+                                                   .append([int(line_gen()),
+                                                            int(line_gen())])
+                        except Exception as e: #TODO
+                            print('inner try: line {} count {}'.format(line,
+                                                                       count))
+                            print(e.message)
+                except Exception as e:
+                    print('outer try: line {} count {}'.format(line, count))
+                    print(e.message)
+            return region_map
 
 IN_GFF = '/disk/bioscratch/Will/Drop_Box/GCF_001266775.1_Austrofundulus_limnaeus-1.0_genomic_andMITO.gff'
 IN_SAM = '/disk/bioscratch/Will/Drop_Box/HPF_small_RNA_022216.sam'
 
 if __name__ == '__main__':
-    print(RegionMap.read_gff(IN_GFF, ['exon']))
+    RegionMap.read_gff(IN_GFF, ['exon'])
 

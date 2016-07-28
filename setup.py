26a8e346e9ae16f75191dfa084fec78a49bb4553
willpatterson <wpatt2@pdx.edu>
2016-07-28 03:25:20 -0700

    Small fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install
 
-setup(name="SamLink",
-      version="0.0.0",
-      description="Pulls relivent data from .sam files and maps sam sequences onto a .gff file",
-      license="MIT",
-      author="William Patterson, Amie Romney",
+setup(name='SamLink',
+      version='0.0.0',
+      description='Pulls relivent data from .sam files and maps sam sequences onto a .gff file',
+      license='MIT',
+      author='William Patterson, Amie Romney',
       packages=find_packages(),
-      install_requires=["BioPython", 'pysam'])
+      install_requires=['pysam'])
 

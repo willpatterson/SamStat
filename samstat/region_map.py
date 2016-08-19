67e765571f67668d42ab385a98a2aac5245bdcf8
willpatterson <wpatt2@pdx.edu>
2016-08-19 16:35:01 -0700

    Moved region map code to its own file

---
+++ samstat/region_map.py
@@ -0,0 +1,102 @@
+""" Region Map
+This file contains the code for reading GFF3 files into region maps
+"""
+import argparse
+import os
+import pysam
+import warnings
+import timeit
+from collections import namedtuple
+from functools import lru_cache
+from operator import itemgetter
+
+def split_gen(s, delims):
+    """iterates a delimited line"""
+    start = 0
+    for i, c in enumerate(s):
+        if c in delims:
+            yield s[start:i]
+            start = i+1
+    yield s[start:]
+
+class RegionMap(object):
+    """Reads creates a feature location map from a gff file that can be
+    used to determine gene attribute types from sequence location ranges
+    """
+    Region = namedtuple('Region', ['features', 'length'])
+    def __init__(self, gff_path, accepted_features='exon'):
+        if isinstance(accepted_features, str):
+            accepted_features = tuple([accepted_features])
+        self.feature_types = accepted_features
+        print(accepted_features)
+        self.rmap = self.read_gff(gff_path, feature_types=self.feature_types)
+
+    @classmethod
+    def read_gff(cls, gff_path, feature_types):
+        """Reads a gff file into a region map hash"""
+        region_map = {}
+        feature_temp = {key: [] for key in feature_types}
+        with open(gff_path, 'r') as gff:
+            for count, line in enumerate(gff):
+                try:
+                    if line.startswith('#'):
+                        continue
+                    line_gen = split_gen(line, '\t ')
+                    region = next(line_gen)
+                    next(line_gen)
+                    feature = next(line_gen)
+                    location = [int(next(line_gen)), int(next(line_gen))]
+                    try:
+                        region_map[region].features[feature].append(location)
+                    except KeyError:
+                        if feature == 'region':
+                            region_map.setdefault(region,
+                                                  cls.Region(feature_temp,
+                                                             location[1]))
+                        else:
+                            region_map.setdefault(region,
+                                                  cls.Region(feature_temp,
+                                                             None))
+                            try:
+                                region_map[region].features[feature]\
+                                                  .append(location)
+                            except KeyError:
+                                pass
+
+                except StopIteration:
+                    warnings.warn('Invalid line: {} ... skipped'.format(count))
+        return cls.calc_missing_region_lengths(region_map)
+
+    @classmethod
+    def calc_missing_region_lengths(cls, region_map):
+        """Calculates region lengths for regions without a region line"""
+        for key, region in region_map.items():
+            if region.length is None:
+                features = iter(region.features)
+                largest = next(features)[1]
+                for feat in features:
+                    if largest < feat[1]: largest = feat[1]
+                region_map[key] = cls.Region(region.features, largest)
+        return region_map
+
+    @lru_cache(maxsize=None)
+    def get_location_clasification(self,
+                                   region_name,
+                                   location_start,
+                                   location_stop):
+        """Gets location classification from region_map"""
+        for key, ranges in self.rmap[region_name].features.items():
+            for feat_range in ranges:
+                start_flag = location_start in range(feat_range[0],
+                                                     feat_range[1])
+                stop_flag = location_stop in range(feat_range[0],
+                                                   feat_range[1])
+
+                if stop_flag is True and start_flag is True:
+                    return key
+                elif stop_flag != start_flag:
+                    return 'combo'
+                else:
+                    return 'intron'
+
+

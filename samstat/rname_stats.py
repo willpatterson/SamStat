aa684689b999a78ebe753dd7da86f7faba1ba2fd
willpatterson <wpatt2@pdx.edu>
2016-08-23 18:57:26 -0700

    Added base of rname_stats.py

---
+++ samstat/rname_stats.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('..')
+
+from maps import RegionMap
+from maps import AlignmentMap
+
+def calculate_statistics(alignment_map, region_map):
+    pass
+
+def run(in_sam, in_gff, outpath):
+    pass
+
+def main(args=None):
+    pass
+
+if __name__ == '__main__':
+    pass

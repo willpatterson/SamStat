369630148f3e9d51a6647178858920d6c881b3de
willpatterson <wpatt2@pdx.edu>
2016-08-26 18:52:11 -0700

    Update base of rname_stats

--- samstat/rname_stats.py
+++ samstat/rname_stats.py
@@ -5,12 +5,16 @@ from maps import RegionMap
 from maps import AlignmentMap
 
 def calculate_statistics(alignment_map, region_map):
-    pass
+    """ """
+    #for qname, qdata in alignment_map.items():
 
 def run(in_sam, in_gff, outpath):
-    pass
+    """Runs the rname stats script"""
+    amap = AlignmentMap(in_sam)
+    rmap = RegionMap(in_gff)
 
 def main(args=None):
+    """Comamnd line argument for rname stats"""
     pass
 
 if __name__ == '__main__':

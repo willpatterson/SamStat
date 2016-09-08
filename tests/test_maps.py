d10c6361716563c6f89d90d58762def390bea29f
willpatterson <wpatt2@pdx.edu>
2016-09-07 18:43:54 -0700

    Added first test for binary_coordnate_match

--- tests/test_maps.py
+++ tests/test_maps.py
@@ -13,9 +13,23 @@ class TestRegionMap(unittest.TestCase):
         cls.rm = RegionMap(IN_GFF)
         cls.rm_list = list(rm.rmap.items())
 
+COORD = (5, 15)
+FULL_MATCH = (5, 15)
+LOWER_MATCH = (10, 20)
+UPPER_MATCH = (1, 10)
+
+
+class TestRegion(unittest.TestCase):
+    """Tests for Region Class"""
+    #def setUpClass(cls):
+    def test_coordinate_relations_full(self):
+        lower, upper = Region.coordinate_relations(COORD, FULL_MATCH)
+        self.assertTrue(lower)
+        self.assertTrue(upper)
+
 
 if __name__ == '__main__':
-    test_classes = (TestRegionMap)
+    test_classes = (TestRegionMap, TestRegion)
     test_suite = unittest.TestSuite()
     for test_class in test_classes:
         test_suite.addTest(test_class())

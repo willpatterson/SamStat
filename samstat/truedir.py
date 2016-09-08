130c5d3049fbc2626225bd8c965b6ba807de735f
willpatterson <wpatt2@pdx.edu>
2016-09-08 03:04:34 -0700

    Added OutLine namedtuple for truedir

--- samstat/truedir.py
+++ samstat/truedir.py
@@ -4,9 +4,13 @@ sys.path.append('..')
 from maps import RegionMap
 from maps import AlignmentMap
 
+OutLine = namedtuple('OutLine', ['qname', 'rname', 'forward', 'reverse'])
+
 def calculate_statistics(alignment_map, region_map):
     """ """
     #for qname, qdata in alignment_map.items():
+    #    for qdata.
+
 
 def run(in_sam, in_gff, outpath):
     """Runs the rname stats script"""
